6155dfab9bfcf1e46f26721613c502684a596b94
Mahrus Ferdous <mahrusferdous@gmail.com>
2024-03-19 11:28:19 +0600

    Add new homework files and make code corrections

---
+++ homework_2.py
@@ -0,0 +1,7 @@
+# task 1 - correct the code 
+Pi_value = 3.14 # not good practice unless for class 
+userAge = 25 # variable
+user_location = "New York" # variable
+MAXLIMIT = 1000 # constant
+
+print(Pi_value , userAge , user_location , MAXLIMIT)

3eab6a016052e83445a6bc9c1e2554ad6586b4dd
Mahrus Ferdous <mahrusferdous@gmail.com>
2024-03-19 11:15:04 +0600

    Add weather and mood conditions

---
+++ homework1.py
@@ -0,0 +1,16 @@
+# Task 1 - correct the code
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+    
+# Task 2 - Mood today
+mood = input("How are you feeling today, happy or sad?\n")
+
+if (mood == "happy"):
+    print("Thats great to hear!")
+else:   
+    print("I hope your day gets better!")
+    
